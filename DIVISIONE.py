cca5226f7199290d6d18116d015f25f4e204e4ad
PietroLastrucci <lastrucci2008@gmail.com>
2021-12-30 14:44:23 +0100

    CODING POLPE

---
+++ DIVISIONE.py
@@ -0,0 +1,10 @@
+#la divisione tra due numeri interi restituisce un float
+a = float(input("A"))
+b = float(input("b"))
+c = a/b
+pcc = "c"
+print("scrivi c")
+
+abc = input("")
+if abc == pcc :
+    print(c)
---
+++ SOTTRAZIONE.py
@@ -0,0 +1,10 @@
+#sottrazione fra due numeri interi restituisce un int
+a = int(input("Primo fattore"))
+b = int(input("Secondo fattore"))
+c = a-b
+pcc = "c"
+print("scrivi c")
+
+abc = input("")
+if abc == pcc :
+    print(c)
